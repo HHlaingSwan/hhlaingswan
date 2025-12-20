7d3699285c0e4cea4b8c5ea300989bd2e5af6629
H Hlaing Swan <168323270+HHlaingSwan@users.noreply.github.com>
2025-12-20 20:42:54 +0630

    Image File Path

--- sections/ProjectOverview.tsx
+++ sections/ProjectOverview.tsx
@@ -1,6 +1,14 @@
 import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 import React from "react";
 import { AuroraText } from "@/components/ui/aurora-text";
+import Image from "next/image";
+
+import IntegrationAPI from "@/assets/images/IntegrationAPI.png";
+import shareLoc from "@/assets/images/shareLoc.png";
+import canViewImage from "@/assets/images/canViewImage.png";
+import canPlayVd from "@/assets/images/canPlayVd.png";
+import cmtImageHoverView from "@/assets/images/cmtImageHoverView.png";
+import arcane from "@/assets/images/arcane.png";
 
 const ProjectOverview = () => {
   const content: any = [
@@ -10,8 +18,8 @@ const ProjectOverview = () => {
         "Successfully integrated a robust login API into the WeDay platform using React, enhancing user authentication, session management, and overall security. This integration streamlines user access and improves data handling efficiency.",
       content: (
         <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/ProjectData/IntegrationAPI.png"
+          <Image
+            src={IntegrationAPI}
             width={300}
             height={300}
             className="h-full w-full object-cover"
@@ -27,8 +35,8 @@ const ProjectOverview = () => {
         "Introduced comprehensive sharing functionalities, allowing users to share posts, broadcast their location, and tag friends. This fosters deeper connections and community interaction, making content sharing more dynamic and personalized.",
       content: (
         <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/ProjectData/shareLoc.png"
+          <Image
+            src={shareLoc}
             width={300}
             height={300}
             className="h-full w-full object-cover"
@@ -44,8 +52,8 @@ const ProjectOverview = () => {
         "Integrated advanced media viewing capabilities, allowing users to view high-resolution photos directly within the application. Optimized for fast loading and a visually rich experience, ensuring all images are displayed with clarity and detail.",
       content: (
         <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/ProjectData/canViewImage.png"
+          <Image
+            src={canViewImage}
             width={300}
             height={300}
             className="h-full w-full object-cover"
@@ -60,8 +68,8 @@ const ProjectOverview = () => {
         "Implemented a seamless video playback experience within the application, enabling users to watch videos without leaving the platform. Features include responsive playback controls and optimized streaming for various devices and network conditions.",
       content: (
         <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/ProjectData/canPlayVd.png"
+          <Image
+            src={canPlayVd}
             width={300}
             height={300}
             className="h-full w-full object-cover"
@@ -76,12 +84,28 @@ const ProjectOverview = () => {
         "Developed a unique feature where hovering over a comment dynamically displays associated photos. This innovative interaction provides context and enriches the comment viewing experience, making discussions more visual and engaging.",
       content: (
         <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/ProjectData/cmtImageHoverView.png"
+          <Image
+            src={cmtImageHoverView}
             width={300}
             height={300}
             className="h-full w-full object-cover"
             alt=" comment hover image view demo"
+          />
+        </div>
+      ),
+    },
+    {
+      title: "  Static Movie Fan Page  Base On Arcane Series",
+      description:
+        "Created a visually captivating static fan page dedicated to the Arcane series, showcasing key characters, episodes, and lore. The page features immersive design elements that celebrate the show's unique art style and storytelling.",
+      content: (
+        <div className="flex h-full w-full items-center justify-center text-white">
+          <Image
+            src={arcane}
+            width={300}
+            height={300}
+            className="h-full w-full object-cover"
+            alt="  arcane demo"
           />
         </div>
       ),
